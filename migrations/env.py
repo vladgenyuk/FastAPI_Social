829708fc61ceab26aa5ad33b04a8527812338117
vlad <vlad.genyuk@mail.ru>
2023-07-08 19:33:10 +0300

    Added register

--- migrations/env.py
+++ migrations/env.py
@@ -5,9 +5,26 @@ from sqlalchemy import pool
 
 from alembic import context
 
+
+import os
+import sys
+
+sys.path.append(os.path.join(sys.path[0], 'src'))
+
+from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+from database import metadata, Base
+from src.auth.models import metadata as metadata_auth
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+section = config.config_ini_section
+config.set_section_option(section, 'DB_HOST', DB_HOST)
+config.set_section_option(section, 'DB_NAME', DB_NAME)
+config.set_section_option(section, 'DB_PASS', DB_PASS)
+config.set_section_option(section, 'DB_PORT', DB_PORT)
+config.set_section_option(section, 'DB_USER', DB_USER)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -18,7 +35,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+
+target_metadata = [metadata_auth]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
